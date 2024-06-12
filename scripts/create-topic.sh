7cd985a98456192c1f1054a37fd60a82a0a954ff
kanakraj <t-kanakraj@microsoft.com>
2024-06-12 12:03:51 +0530

    changed the container name in create-topic.sh

--- scripts/create-topic.sh
+++ scripts/create-topic.sh
@@ -2,7 +2,7 @@
 
 topic=$1
 
-docker exec -i pwr-studio-release-kafka-1 kafka-topics.sh --create --bootstrap-server localhost:9092 --topic $topic
+docker exec -i pwr-studio-kafka-1 kafka-topics.sh --create --bootstrap-server localhost:9092 --topic $topic
 
 
 
