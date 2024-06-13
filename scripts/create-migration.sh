c6a78001bdb1d5bfbfc6f474aa2526fab07af31d
wi0lono <atharv.kirtikar@gmail.com>
2024-06-13 18:13:44 +0530

    Fix misguided scripts

--- scripts/create-migration.sh
+++ scripts/create-migration.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+msg=$1
+
 python3 -m venv .venv
 source .venv/bin/activate
 pip3 install alembic psycopg2-binary
@@ -12,7 +14,7 @@ if grep -qi microsoft /proc/version && grep -q WSL2 /proc/version; then
     echo "Setting Kakfa & Postgres host by WSL2 IP: ${PWRHOST}"
 else
     export PWR_KAFKA_BROKER=kafka:9092
-    export POSTGRES_DATABASE_HOST=localhost
+    export POSTGRES_DATABASE_HOST=postgres
     echo "Setting Kakfa & Postgres host by docker-compose service name: kafka, postgres"
 fi
 
@@ -21,4 +23,4 @@ set -a
 source .env-dev
 set +a
 
-alembic upgrade head
+alembic revision --autogenerate -m "$msg"
