aca8616418b65801966090ff06ae7731ebfc2a2f
kanak8278 <kanak8278@gmail.com>
2024-08-06 15:42:53 +0530

    feat: Add lazy loading for images in DevBotFooter component

--- studio/src/components/Chat/DevBotFooter.tsx
+++ studio/src/components/Chat/DevBotFooter.tsx
@@ -2,6 +2,7 @@ import '@/styles/components/DevBotFooter.scss';
 import { DefaultButton, IconButton, Stack, TextField, makeStyles } from "@fluentui/react";
 import { useTranslation } from 'react-i18next';
 import React from "react";
+import useConfig from '../../hooks/useConfig';
 
 
 interface props {
@@ -23,6 +24,7 @@ export const DevBotFooter = (props: props) => {
     const classStyles = devbotfooterStyles();
     const { t, i18n} = useTranslation();
     const [message, setMessage] = React.useState('');
+    const config = useConfig();
 
     React.useEffect(() => { 
         if (props.inputText) {
@@ -37,15 +39,63 @@ export const DevBotFooter = (props: props) => {
         }
     }, [props.inputText]);
 
+    if (!config) {
+        return (
+            <Stack className='devbot-footer'>
+                <Stack.Item>
+                    <Stack horizontal className='button-stack' tokens={{childrenGap: 20}}>
+                        <Stack.Item>
+                            <input ref={fileInput} accept=".pdf, .doc, .docx, .xls, .xlsx, .ppt, .pptx, .txt, .rtf, .odt" onChange={props.onFileChange} type='file' multiple hidden />
+                            <DefaultButton  onClick={() => { window.event?.stopImmediatePropagation(); fileInput?.current?.click(); }} className='primary-button'>{t('devFooter.uploadFiles')}</DefaultButton>
+                        </Stack.Item>
+                        <Stack.Item>
+                            <DefaultButton onClick={() => props.pluginStoreToggle()} className='primary-button'>{t('devFooter.addPlugin')}</DefaultButton>
+                        </Stack.Item>
+                    </Stack>
+                </Stack.Item>
+                <Stack.Item>
+                    <Stack className='input-field-stack'>
+                        <Stack.Item>
+                            <TextField 
+                                maxLength={200000}
+                                value={message}
+                                onKeyUp={(e) => { 
+                                        if (props.disableSend) { return; }
+                                        if (e.key === 'Enter') {
+                                            props.sendMessageToWss(message);
+                                            setMessage('');
+                                        } 
+                                    }
+                                }
+                                onChange={(e, value) => { setMessage(value || '') }}
+                                type="text"
+                                placeholder="Type your message here..."
+                                multiline={true}
+                                resizable={false}
+                                onRenderSuffix={() => {
+                                   return (<IconButton 
+                                        disabled={props.disableSend}
+                                        onClick={() => { if (props.disableSend) { return; } props.sendMessageToWss(message); setMessage('')  }}
+                                        iconProps={{'iconName': 'Send'}} title="Emoji" ariaLabel="Emoji" />) 
+                                }}
+                                />
+                        </Stack.Item>
+                    </Stack>
+    
+                </Stack.Item>
+            </Stack>
+        )
+    }
 
     return (
         <Stack className='devbot-footer'>
             <Stack.Item>
                 <Stack horizontal className='button-stack' tokens={{childrenGap: 20}}>
-                    <Stack.Item>
-                        <input ref={fileInput} accept=".pdf, .doc, .docx, .xls, .xlsx, .ppt, .pptx, .txt, .rtf, .odt" onChange={props.onFileChange} type='file' multiple hidden />
-                        <DefaultButton  onClick={() => { window.event?.stopImmediatePropagation(); fileInput?.current?.click(); }} className='primary-button'>{t('devFooter.uploadFiles')}</DefaultButton>
-                    </Stack.Item>
+                        {config.features["devFooter.uploadFiles"] && <Stack.Item>
+                            <input ref={fileInput} accept=".pdf, .doc, .docx, .xls, .xlsx, .ppt, .pptx, .txt, .rtf, .odt" onChange={props.onFileChange} type='file' multiple hidden />
+                            <DefaultButton  onClick={() => { window.event?.stopImmediatePropagation(); fileInput?.current?.click(); }} className='primary-button'>{t('devFooter.uploadFiles')}</DefaultButton>
+                        </Stack.Item>
+                        }
                     <Stack.Item>
                         <DefaultButton onClick={() => props.pluginStoreToggle()} className='primary-button'>{t('devFooter.addPlugin')}</DefaultButton>
                     </Stack.Item>
